a63f41cb80490adf6fb6d110b1a4797aca298e9b
Abhay kumar srivastava <120777323+srivastavm089@users.noreply.github.com>
2023-04-30 18:31:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-//your JS code here. If required.
+const rend = document.getElementById('timer')
+const date = new Date();
+let da = date.getDate();
+let month = date.getMonth();
+let year = date.getFullYear();
+let hours = date.getHours();
+let minute = date.getMinute();
+let second = date.getSeconds();
+rend.innerText = `${da}/${month}/${year}, ${hours}:${minute}:${second}`;
+
