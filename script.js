1c457b340d7245b79a8fa7b9261b208f48a29a85
Abhay kumar srivastava <120777323+srivastavm089@users.noreply.github.com>
2023-05-08 13:20:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
-const rend = document.getElementById('timer')
-const date = new Date();
-let da = date.getDate();
-let month = date.getMonth();
-let year = date.getFullYear();
-let hours = date.getHours();
-let minute = date.getMinute();
-let second = date.getSeconds();
-rend.innerText = `${da}/${month}/${year}, ${hours}:${minute}:${second}`;
+let target = document.getElementById('timer');
 
+function updateTimer() {
+  let date = new Date();
+  let day = date.getDate().toString().padStart(2, '0');
+  let month = (date.getMonth() + 1).toString().padStart(2, '0');
+  let year = date.getFullYear();
+  let hours = date.getHours().toString().padStart(2, '0');
+  let minute = date.getMinutes().toString().padStart(2, '0');
+  let second = date.getSeconds().toString().padStart(2, '0');
+  let timer = `${day}/${month}/${year}, ${hours}:${minute}:${second}`;
+  target.innerText = timer;
+}
+
+setInterval(updateTimer, 1000);
